0aa3c78c24a106ce430bfc898a4a010e7242436e
loic.huart <loic.huart08@gmail.com>
2024-05-16 22:24:18 +0200

    feat: add seeder

---
+++ seeder/seed.sh
@@ -0,0 +1,6 @@
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection ingredients --file ./seeder/data/ingredients.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection recipes --file ./seeder/data/recipes.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection orders --file ./seeder/data/orders.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection machineconfigurations --file ./seeder/data/machineconfigurations.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection files --file ./seeder/data/files.json --jsonArray
+cp -r ./seeder/uploads ./public

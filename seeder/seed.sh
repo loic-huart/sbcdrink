b3f8326e36b304ef7fa28d63bb2eeb29477a0984
loic.huart <loic.huart08@gmail.com>
2024-09-09 21:57:28 +0200

    feat: update seeder

--- seeder/seed.sh
+++ seeder/seed.sh
@@ -1,7 +1,9 @@
-mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection ingredients --file ./seeder/data/ingredients.json --jsonArray
-mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection recipes --file ./seeder/data/recipes.json --jsonArray
-mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection orders --file ./seeder/data/orders.json --jsonArray
-mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection machineconfigurations --file ./seeder/data/machineconfigurations.json --jsonArray
-mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection files --file ./seeder/data/files.json --jsonArray
-mkdir -p ./build/public/uploads
-cp -r ./seeder/uploads ./build/public
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection Ingredient --file ./seeder/data/ingredient.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection Recipe --file ./seeder/data/recipe.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection RecipeStep --file ./seeder/data/recipeStep.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection Order --file ./seeder/data/order.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection OrderStep --file ./seeder/data/orderStep.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection Machineconfiguration --file ./seeder/data/machineconfiguration.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection File --file ./seeder/data/file.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection Setting --file ./seeder/data/setting.json --jsonArray
+cp -r ./seeder/uploads ./public

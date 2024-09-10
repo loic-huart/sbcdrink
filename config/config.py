263d85b49b4041f6ed6dee93a78b9a8bb274735f
loic huart <loic.huart08@gmail.com>
2024-09-10 16:55:37 +0000

    fix merge conflit

--- config/config.py
+++ config/config.py
@@ -23,23 +23,4 @@ BELT_LIMIT_SENSOR = {
     "PIN_SIGNAL": config["belt_limit_sensor"]["pin_signal"],
 }
 
-BOTTLE_SLOT_POSITION = {
-    "SLOT_1": config["bottle_slot_position"]["slot_1"],
-    "SLOT_2": config["bottle_slot_position"]["slot_2"],
-    "SLOT_3": config["bottle_slot_position"]["slot_3"],
-    "SLOT_4": config["bottle_slot_position"]["slot_4"],
-    "SLOT_5": config["bottle_slot_position"]["slot_5"],
-    "SLOT_6": config["bottle_slot_position"]["slot_6"],
-    "SLOT_7": config["bottle_slot_position"]["slot_7"],
-    "SLOT_8": config["bottle_slot_position"]["slot_8"],
-    "SLOT_9": config["bottle_slot_position"]["slot_9"],
-    "SLOT_10": config["bottle_slot_position"]["slot_10"],
-    "SLOT_11": config["bottle_slot_position"]["slot_11"],
-    "SLOT_12": config["bottle_slot_position"]["slot_12"],
-}
-
-TIME_FOR_ONE_QUANTITY = config["time_for_one_quantity"]
-
-TIME_FOR_FILL_DISPENSOR = config["time_for_fill_dispensor"]
-
 PRESENT_POSITION = config["present_position"]
